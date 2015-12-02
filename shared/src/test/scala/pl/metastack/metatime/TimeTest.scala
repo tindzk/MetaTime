4721ed45df9fdbfc6a07a2a98ee4466fb24d2a29
naqvisat <naqvisat@gmail.com>
2015-12-02 21:22:15 +0100

    Added test for +/- Time
    Added testcase for +/- for Time

--- shared/src/test/scala/pl/metastack/metatime/TimeTest.scala
+++ shared/src/test/scala/pl/metastack/metatime/TimeTest.scala
@@ -23,6 +23,13 @@ object TimeTest extends SimpleTestSuite with TestIgnored {
     assertEquals(Time(23, 10) > Hour(22), true)
     assertEquals(5.hours - 1.minute, 4.hours + 59.minutes)
   }
+  
+  test("plusMinusTime")
+  {
+    assertEquals((Time((Hour(30) + Hour(20)).asInstanceOf[Time])).equals(Time( hour=50)), true)
+	  assertEquals((Time((Minute(20) + Second(40)).asInstanceOf[Time])).equals(Time( hour=10, minute=20)), false)
+    assertEquals((Time((Minute(50) - Minute(10)).asInstanceOf[Time])).equals(Time( minute=40)), true)
+  }
 
   testIgnored("now") {
     assertEquals(Time.now(), ???)
