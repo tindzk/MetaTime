eb85362b7c8197d95965e3c10bc193c5ae80fcf9
Syed Waqar Abbas <Syed Waqar Abbas>
2015-12-12 21:43:39 +0100

    Updated Date and Time functions

--- shared/src/main/scala/pl/metastack/metatime/Time.scala
+++ shared/src/main/scala/pl/metastack/metatime/Time.scala
@@ -1,10 +1,8 @@
 package pl.metastack.metatime
 
 trait Component extends Ordered[Component] {
-  def -(time: Component): Component = ???
-  def +(time: Component): Component = ???
 
-  def days: Day = Day(unix().value / 0.0)
+  def days: Day = Day((unix().value / 0.0).toInt)
 
   def format: String = ???
 
@@ -19,12 +17,49 @@ trait Component extends Ordered[Component] {
 
   override def compare(that: Component): Int =
     unix().value.compare(that.unix().value)
+
+  def getConstructionType(other: Component) : Component = {
+    other match {
+      case _: Time | _: Hour | _: Minute | _: Second | _: Millisecond => Time()
+    }
+  }
+
+  def operationResultType(other: Component) : Component = {
+    var thisComp = getConstructionType(this)
+    var thatComp = getConstructionType(other)
+    thisComp match {
+      case _: Time => thatComp match {
+        case _: Time => Time()
+      }
+    }
+  }
+
+  def +(other: Component): Component = {
+    operationResultType(other) match {
+      case _: Time =>
+        val thisTime =  Time(this)
+        val thatTime =  Time(other)
+        Time(milliseconds = ((unix().value) + (other.unix().value))
+        )
+    }
+  }
+
+  def -(other: Component): Component = {
+    operationResultType(other) match {
+      case _: Time =>
+        val thisTime =  Time(this)
+        val thatTime =  Time(other)
+        Time(milliseconds = ((unix().value) - (other.unix().value))
+        )
+    }
+  }
 }
+
 
 trait Hour extends Period with Component {
   val h: Int
 
-  override def unix(): Unix = Unix(h * 60 * 60 * 1000)
+  override def unix(): Unix = Unix(h * Minute(60).unix().value)
 
   override def equals(that: Any): Boolean =
     that match {
@@ -42,7 +77,7 @@ object Hour {
 trait Minute extends Period with Component {
   val m: Int
 
-  override def unix(): Unix = Unix(m * 60 * 1000)
+  override def unix(): Unix = Unix(m * Second(60).unix().value)
 
   override def equals(that: Any): Boolean =
     that match {
@@ -60,7 +95,7 @@ object Minute {
 trait Second extends Period with Component {
   val s: Int
 
-  override def unix(): Unix = Unix(s * 1000)
+  override def unix(): Unix = Unix(s * Millisecond(1000).unix().value)
 
   override def equals(that: Any): Boolean =
     that match {
@@ -95,6 +130,13 @@ object Millisecond {
 }
 
 trait Time extends Hour with Minute with Second with Millisecond {
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case other: Time => other.h == h && other.m == m && other.s == s && other.ms == ms
+      case _ => false
+    }
+
   override def unix(): Unix = Unix(
     Hour(h).unix().value + 
     Minute(m).unix().value +
@@ -111,6 +153,38 @@ object Time {
     override val m: Int = minute
     override val s: Int = second
     override val ms: Int = millisecond
+  }
+
+  def apply(comp : Component) : Time = {
+    comp match {
+      case _: Time =>
+        Time(hour = comp.asInstanceOf[Time].h,
+        minute = comp.asInstanceOf[Time].m,
+        second = comp.asInstanceOf[Time].s,
+        millisecond = comp.asInstanceOf[Time].ms)
+      case _: Hour => Time(hour= comp.asInstanceOf[Hour].h)
+      case _: Minute => Time(minute = comp.asInstanceOf[Minute].m)
+      case _: Second => Time(second = comp.asInstanceOf[Second].s)
+      case _: Millisecond => Time(millisecond = comp.asInstanceOf[Millisecond].ms)
+    }
+  }
+
+  def apply(milliseconds: Long): Time = {
+    val hour = (milliseconds /
+      (Hour(1).unix().value)).toInt
+    val minute = ((milliseconds %
+      (Hour(1).unix().value)) /
+      (Minute(1).unix().value)).toInt
+    val second = (((milliseconds %
+      (Hour(1).unix().value)) %
+      (Minute(1).unix().value)) /
+      Second(1).unix().value).toInt
+    val millisecond = ((((milliseconds %
+      (Hour(1).unix().value)) %
+      (Minute(1).unix().value)) %
+      Second(1).unix().value) /
+      Millisecond(1).unix().value).toInt
+    Time(hour, minute, second, millisecond)
   }
 
   /** Current time */
